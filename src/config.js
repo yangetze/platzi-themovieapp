2a137e72e811c9bc3f14350a88c8e69be8ae011c
Yang <yangetze@gmail.com>
2022-10-02 17:53:23 -0400

    Se agrega variable con los headers para axios

--- src/config.js
+++ src/config.js
@@ -2,3 +2,10 @@ var config = {
     API_KEY : '7d294dcb1101db79d260742cc1fdf3e7',
     TOKEN_AUTH_V4 : 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDI5NGRjYjExMDFkYjc5ZDI2MDc0MmNjMWZkZjNlNyIsInN1YiI6IjYzMTk1ZDA3OGRkYzM0MDA3YTYxYzM1MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.m4L6kbQxv_YMzNwNXTyyiWwqAPOKSOvqCb-ISjvoqFw'
   }
+
+  const httpHeaders = {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "text/plain",
+    },
+  };
